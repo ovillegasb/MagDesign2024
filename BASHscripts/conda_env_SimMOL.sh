549668100a58370ace2e0e5fd18a41fdc108dded
Orlando Villegas <ovillegas.bello0317@gmail.com>
2024-03-26 11:38:26 +0100

    update SimMOL env

--- BASHscripts/conda_env_SimMOL.sh
+++ BASHscripts/conda_env_SimMOL.sh
@@ -3,14 +3,42 @@
 # Creation and configuration of the SimMOL working environment in Anaconda.
 
 # Enviroment SimMOL
-conda create --name SimMOL scipy jupyterlab numpy matplotlib seaborn pandas
+conda create --name SimMOL
 conda activate SimMOL
 conda config --add channels conda-forge
 
+# Basic modules
+conda install anaconda::numpy
+conda install anaconda::pandas
+conda install anaconda::scipy
+conda install conda-forge::matplotlib
+conda install anaconda::seaborn
+
+conda install conda-forge::notebook
+conda install conda-forge::jupyter
+conda install conda-forge::jupyterlab
+conda install conda-forge::jupyter_contrib_nbextensions
+conda install conda-forge::jupyter-lsp
+
 # Useful modules
-conda install pymatgen
-conda install mdtraj
-conda install mdanalysis
-conda install networkx
-conda install statsmodels
-conda install rdkit
+conda install anaconda::networkx
+conda install conda-forge::statsmodels
+conda install conda-forge::pymatgen # or -c conda-forge pymatgen
+conda install conda-forge::mdtraj
+conda install conda-forge::mdanalysis
+conda install conda-forge::rdkit
+conda install conda-forge::nglview
+conda install conda-forge::ase
+
+# Some jupyter commands
+jupyter --paths
+jupyter --config-dir
+
+# Some conda commands
+conda config --show [or channels]
+
+# Some python modules
+python -m site --user-site
+
+# To remove
+conda env remove --name SimMOL
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # MagDesign2024
+
+Useful scripts of various tools and languages.
+
+
+## Scripts included in `MagDesign2024` repertoire
+
+### `./BASHscripts`
+
+BASH script compilation, help to configure or automate some tasks.
+
+- `conda_env_SimMOL.sh` Useful commands used to configure with SimMOL env conda.
+- `jupyterLab_server.sh` Run jupyterlab in the system backgroud.
