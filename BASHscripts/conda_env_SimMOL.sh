968cae5c4e7d57276cad775586dd729fa65e271d
Orlando Villegas <ovillegas.bello0317@gmail.com>
2024-04-04 14:45:16 +0200

    Corrections

--- BASHscripts/conda_env_SimMOL.sh
+++ BASHscripts/conda_env_SimMOL.sh
@@ -12,31 +12,28 @@ conda install anaconda:pip
 conda install anaconda::numpy
 conda install anaconda::pandas
 conda install anaconda::scipy
-conda install conda-forge::matplotlib
+conda install -c conda-forge matplotlib
 conda install anaconda::seaborn
 conda install anaconda::networkx
 
 # For notebooks
 conda install -c conda-forge jupyterlab
 conda install -c conda-forge jupyter_contrib_nbextensions
-## conda install -c conda-forge notebook
-## conda install -c conda-forge jupyter_contrib_nbextensions
 ## conda install -c conda-forge widgetsnbextension
 ## conda install -c conda-forge jupyter-lsp
-## conda install conda-forge::jupyter
-## conda install conda-forge::jupyterlab
+
 
 # Useful modules
 conda install conda-forge::statsmodels
-conda install conda-forge::pymatgen # or -c conda-forge pymatgen
+conda install -c conda-forge pymatgen
 conda install conda-forge::mdtraj
 conda install conda-forge::mdanalysis
 conda install conda-forge::rdkit
 conda install conda-forge::ase
 
 # For NGLview
-pip install nglview
-conda install anaconda::ipywidgets
+## pip install nglview
+## conda install anaconda::ipywidgets
 ## jupyter-nbextension enable --py --sys-prefix widgetsnbextension
 ## jupyter-nbextension enable --py --sys-prefix nglview
 
@@ -47,8 +44,6 @@ jupyter --config-dir
 jupyter-nbextension list
 jupyter-labextension update --all
 jupyter-labextension list
-jupyter-labextension install @jupyterlab/celltags
-jupyter-serverextension enable --py jupyterlab --sys-prefix
 
 # Some conda commands
 conda config --show [or channels]
